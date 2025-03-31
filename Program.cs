bd2417d9a99ed8f9f4ef6830c39c8ee3354fb3c6
Gustavo Gonzalez <gustavorgonzalezgja@gmal.com>
2025-03-31 19:57:26 -0300

    forca

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string letra, forca;
+Console.WriteLine ("Escreva uma Palavra: ");
+forca = Console.ReadLine().ToUpper();
+Console.WriteLine ("Escreva uma Letra da Forca ");
+letra = Console.ReadLine().ToUpper();
+Console.WriteLine($"Essa letra {letra} esta em {forca}? \n{forca.Contains(letra)}");
